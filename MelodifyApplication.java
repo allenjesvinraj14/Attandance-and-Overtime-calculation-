d6b9acf1aaa27ebc5d85885b135baf042b689d20
allenjesvinraj14 <102140441+allenjesvinraj14@users.noreply.github.com>
2026-06-06 01:14:06 +0530

    Add files via upload

---
+++ MelodifyApplication.java
@@ -0,0 +1,11 @@
+package com.example.melodify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodifyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MelodifyApplication.class, args);
+    }
+}
